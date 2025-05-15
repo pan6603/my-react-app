e51b7de2ad1d3f3637bff6492f8745730d8b24dd
pan6603 <pan5158a@gmail.com>
2025-05-15 20:40:32 +0900

    카카오 헤더 생성

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
-import {ReactComponent as Logo} from "./img/logo/ico_logo.svg";
+import {ReactComponent as IconEarth} from "./img/area/area-earth-icon.svg";
+import {ReactComponent as IconMoon} from "./img/area/area-moon-icon.svg";
+import {ReactComponent as IconMagnifier} from "./img/area/area-magnifier-icon.svg";
+import {ReactComponent as KakaoLogoIcon} from "./img/logo/kakao_logo_icon.svg";
 
 const HeaderWrapper = styled.header`
     max-width:1440px;
@@ -11,55 +14,104 @@ const InnerHeader = styled.div`
     max-width:1316px;
     width:100%;
     height:91px;
-    background-color:green;
     margin: 0 auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
 `;
 
-const MainLogo = styled.h1`
+const KakaoMainLogo = styled.h1`
     max-width: 74px;
     width:100%;
-    height: 24px;
-`;
-
-const MainLogoText = styled.a`
-    max-width: 74px;
-    width:100%;
-    height: 24px;
-    text-decoration: none;
-    color: #333;
+    height: 50px;
     display: flex;
+    justify-content: center;
+    align-items: center;
+` 
+
+const KakaoMainLogoImg = styled(KakaoLogoIcon)`
+    max-width: 74px;
+    width: 100%;
+    height: 50px;
+`
+
+const Navigation = styled.nav`
+    max-width: 666px;
+    width: 100%;
+    height: 37px;
+    display: flex;
+    justify-content: center;
     align-items: center;
 `;
 
-const MainLogoSvg = styled.a`
-    max-width: 74px;
-    width:100%;
-    height: 24px;
-`;
-
-const StyledLogo = styled(Logo)`
-
-`
-const Navigation = styled.nav`
+const NavigationList = styled.ul`
     max-width: 626px;
     width: 100%;
     height: 37px;
-    background-color:blue;
+    margin: 0 auto;
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
+
+const NavItemMenu = styled.li`
+    margin: 0;
+    font-weight: 400;
+`;
+
+
+const AreaUtil = styled.div`
+    max-width: 134px;
+    width: 100%;
+    height: 36px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const IconEarthItem = styled(IconEarth)`
+    max-width: 22px;
+    width: 100%;
+    height: 22px; 
+`
+
+const IconMoonItem = styled(IconMoon)`
+    max-width: 22px;
+    width: 100%;
+    height: 22px; 
+` 
+
+const IconMagnifierItem = styled(IconMagnifier)`
+    max-width: 22px;
+    width: 100%;
+    height: 22px; 
+`
+
+
 
 function Header () {
     return (  
         <HeaderWrapper>
             <InnerHeader>
-                <MainLogo>
-                    <MainLogoText>
-                       <StyledLogo />
-                       <Navigation />
-                    </MainLogoText>
-                </MainLogo>
+                <KakaoMainLogo>
+                    <KakaoMainLogoImg />
+                </KakaoMainLogo>
+                <Navigation>
+                    <NavigationList>
+                        <NavItemMenu>소개</NavItemMenu>
+                        <NavItemMenu>기술과 서비스</NavItemMenu>
+                        <NavItemMenu>약속과 책임</NavItemMenu>
+                        <NavItemMenu>소식</NavItemMenu>
+                        <NavItemMenu>투자정보</NavItemMenu>
+                    </NavigationList>
+                </Navigation>
+
+                <AreaUtil>
+                    <IconMagnifierItem />
+                    <IconEarthItem />
+                    <IconMoonItem />
+                </AreaUtil>
             </InnerHeader>
         </HeaderWrapper>
     ) 
