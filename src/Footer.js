2dca714d7ca33aabce543440da5498163b9bbd87
pan6603 <pan5158a@gmail.com>
2025-05-16 19:42:51 +0900

    innerFooter 레이아웃 생성

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,45 @@ const FooterContainer = styled.footer`
     max-width: 1440px;
     width: 100%;
     height: 634px;
-    background-color: gray;
+    background-color: #FFFFFF;
+    border-top: 1px solid #6D6D6D;
+`
+
+const InnerFooter = styled.div`
+    max-width: 1316px;
+    width: 100%;
+    height: 633px;
+    margin: 0 auto;
+    padding: 81px 0 48px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const SectionService = styled.section`
+    max-width: 1316px;
+    width: 100%;
+    height: 319px;
+    background-color: red;
+    margin: 0 auto;
+`
+
+const SectionRelation = styled.section`
+    max-width: 1316px;
+    width: 100%;
+    height: 65px;
+    background-color: red;
+    margin: 0 auto;
 `
 
 function Footer () {
 
     return (
         <FooterContainer>
-            <p>Hello Footer</p>
+            <InnerFooter>
+                <SectionService></SectionService>
+                <SectionRelation></SectionRelation>
+            </InnerFooter>
         </FooterContainer>
  
     )
--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,6 @@ const MainContainer = styled.main`
     max-width:1440px;
     width: 100%;
     height: 5457px;
-    background-color: blue;
     padding-top: 91px;
 `
 
