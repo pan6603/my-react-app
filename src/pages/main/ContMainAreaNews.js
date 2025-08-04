461654b278346a7bf5a3d331e04f28e6706e48ba
pan6603 <pan5158a@gmail.com>
2025-08-04 22:53:15 +0900

    오늘의 뉴스

--- src/pages/main/ContMainAreaNews.js
+++ src/pages/main/ContMainAreaNews.js
@@ -1,9 +1,227 @@
-import { ContMainAreaNewsContainer } from "../../styles/main/ContMainAreaNews.styles";
+import { 
+    ContMainAreaNewsContainer,
+    TitleContTypeNewsTitNews3,
+    InnerMainInnerNews,
+    WrapTop,
+    WrapBottom,
+    SlickSlider,
+    SlickList,
+    SlickTrack,
+    SlickSlide,
+    SlickSlideDiv1,
+    SlickSlideDiv2,
+    WrapCardInnerSlide,
+    ItemCardNewItemNormalCardTypeMain,
+    InnerCard,
+    InfoCate,
+    IcoCate,
+    TxtCate,
+    TxtDate,
+    BtnShareIcoComm,
+    LinkItemDiv,
+    LinkItem,
+    TitCard,
+    InfoCard,
+    TxtKeyword,
+    WrapThumb,
+    ThumbImg,
+    WrapSlides,
+    WrapLinks,
+    LinkNewsDiv,
+    LinkNews,
+    TitleLink,
+    TodayNewsSlickTrack,
+    TodayNewsSlickSlide,
+    ItemCardNewBoxNews,
+    BoxTop,
+    BadgeBox,
+    IconBox,
+    CateBox,
+    DateBox,
+    TodayNewsBtnShareIcoComm,
+    TitleBox,
+    TitleBoxLink
+} from "../../styles/main/ContMainAreaNews.styles";
 
 
 function ContMainAreaNews() {
     return (
-        <ContMainAreaNewsContainer>오늘의 뉴스</ContMainAreaNewsContainer>
+        <ContMainAreaNewsContainer>
+            <TitleContTypeNewsTitNews3>
+                오늘의 뉴스
+            </TitleContTypeNewsTitNews3>
+            <InnerMainInnerNews>
+                <WrapTop>
+                    <SlickSlider>
+                        <SlickList>
+                            <SlickTrack>
+                                <SlickSlide>
+                                    <SlickSlideDiv1>
+                                        <SlickSlideDiv2>
+                                            <WrapCardInnerSlide>
+                                                <ItemCardNewItemNormalCardTypeMain>
+                                                    <InnerCard>
+                                                        <InfoCate>
+                                                            <IcoCate src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/category/1ee6415a019300001.png?type=thumb&opt=C72x72.fwebp"/>
+                                                            <TxtCate>보도자료</TxtCate>
+                                                            <TxtDate>2025.07.31</TxtDate>
+                                                        </InfoCate>
+                                                        <LinkItemDiv>
+                                                            <LinkItem>
+                                                                <TitCard>카카오프렌즈, 여름 한정 테마 <br /> '냥냥특집' 테마 굿즈 출시</TitCard>
+                                                                <WrapThumb>
+                                                                    <ThumbImg src="https://t1.kakaocdn.net/thumb/C630x354.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fnews%2F5de0d0eb019800001.jpg"/>
+                                                                </WrapThumb>
+                                                            </LinkItem>
+                                                            <InfoCard>
+                                                                <TxtKeyword>#카카오프렌즈</TxtKeyword>
+                                                                <TxtKeyword>#카카오프렌즈 스토어</TxtKeyword>
+                                                                <TxtKeyword>#캐릭터</TxtKeyword>
+                                                                <TxtKeyword>#IP</TxtKeyword>
+                                                                <TxtKeyword>#춘식</TxtKeyword>
+                                                            </InfoCard>
+                                                        </LinkItemDiv>
+                                                    </InnerCard>
+                                                    <BtnShareIcoComm>
+                                                    <svg data-v-19bdf39b="" data-v-76d2af19="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico_share"><g data-v-19bdf39b="" fill="none" fill-rule="evenodd"><path data-v-19bdf39b="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-19bdf39b="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill" fill="black"><circle data-v-19bdf39b="" cx="2" cy="2" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="10" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                                    </BtnShareIcoComm>
+                                                </ItemCardNewItemNormalCardTypeMain>  
+                                            </WrapCardInnerSlide>
+                                        </SlickSlideDiv2>
+                                    </SlickSlideDiv1>
+                                </SlickSlide>
+                                <SlickSlide>
+                                    <SlickSlideDiv1>
+                                        <SlickSlideDiv2>
+                                            <WrapCardInnerSlide>
+                                                <ItemCardNewItemNormalCardTypeMain>
+                                                    <InnerCard>
+                                                        <InfoCate>
+                                                            <IcoCate src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/category/1ee6415a019300001.png?type=thumb&opt=C72x72.fwebp"/>
+                                                            <TxtCate>보도자료</TxtCate>
+                                                            <TxtDate>2025.07.31</TxtDate>
+                                                        </InfoCate>
+                                                        <LinkItemDiv>
+                                                            <LinkItem>
+                                                                <TitCard>카카오, ‘제12회 브런치북 출판 <br /> 프로젝트’ 대상 수상작 출간</TitCard>
+                                                                <WrapThumb>
+                                                                    <ThumbImg src="https://t1.kakaocdn.net/thumb/C630x354.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fnews%2F73e36256019800001.png"/>
+                                                                </WrapThumb>
+                                                            </LinkItem>
+                                                            <InfoCard>
+                                                                <TxtKeyword>#브런치북</TxtKeyword>
+                                                                <TxtKeyword>#브런치스토리</TxtKeyword>
+                                                                <TxtKeyword>#카카오</TxtKeyword>
+                                                            </InfoCard>
+                                                        </LinkItemDiv>
+                                                    </InnerCard>
+                                                    <BtnShareIcoComm>
+                                                    <svg data-v-19bdf39b="" data-v-76d2af19="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico_share"><g data-v-19bdf39b="" fill="none" fill-rule="evenodd"><path data-v-19bdf39b="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-19bdf39b="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill" fill="black"><circle data-v-19bdf39b="" cx="2" cy="2" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="10" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                                    </BtnShareIcoComm>
+                                                </ItemCardNewItemNormalCardTypeMain>
+                                            </WrapCardInnerSlide>
+                                        </SlickSlideDiv2>
+                                    </SlickSlideDiv1>
+                                </SlickSlide>
+                                <SlickSlide>
+                                    <SlickSlideDiv1>
+                                        <SlickSlideDiv2>
+                                            <WrapCardInnerSlide>
+                                                <ItemCardNewItemNormalCardTypeMain>
+                                                    <InnerCard>
+                                                        <InfoCate>
+                                                            <IcoCate src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/category/1ee6415a019300001.png?type=thumb&opt=C72x72.fwebp"/>
+                                                            <TxtCate>보도자료</TxtCate>
+                                                            <TxtDate>2025.07.31</TxtDate>
+                                                        </InfoCate>
+                                                        <LinkItemDiv>
+                                                            <LinkItem>
+                                                                <TitCard>카카오, 신규 톡디지털카드 <br /> ‘러너스 카드’ 출시</TitCard>
+                                                                <WrapThumb>
+                                                                    <ThumbImg src="https://t1.kakaocdn.net/thumb/C630x354.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fnews%2F5e077e51019800001.jpg"/>
+                                                                </WrapThumb>
+                                                            </LinkItem>
+                                                            <InfoCard>
+                                                                <TxtKeyword>#카카오</TxtKeyword>
+                                                                <TxtKeyword>#카카오톡</TxtKeyword>
+                                                                <TxtKeyword>#톡디지털카드</TxtKeyword>
+                                                                <TxtKeyword>#러너스 카드</TxtKeyword>
+                                                                <TxtKeyword>#러너블</TxtKeyword>
+                                                            </InfoCard>
+                                                        </LinkItemDiv>
+                                                    </InnerCard>
+                                                    <BtnShareIcoComm>
+                                                    <svg data-v-19bdf39b="" data-v-76d2af19="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico_share"><g data-v-19bdf39b="" fill="none" fill-rule="evenodd"><path data-v-19bdf39b="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-19bdf39b="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill" fill="black"><circle data-v-19bdf39b="" cx="2" cy="2" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="10" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                                    </BtnShareIcoComm>
+                                                </ItemCardNewItemNormalCardTypeMain>
+                                            </WrapCardInnerSlide>
+                                        </SlickSlideDiv2>
+                                    </SlickSlideDiv1>
+                                </SlickSlide>
+                            </SlickTrack>
+                        </SlickList>
+                    </SlickSlider>
+                </WrapTop>
+                <WrapBottom>
+                    <WrapSlides>
+                        <TodayNewsSlickTrack>
+                            <TodayNewsSlickSlide>
+                                <ItemCardNewBoxNews>
+                                    <BoxTop>
+                                        <BadgeBox>
+                                            <IconBox src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/category/1ee6415a019300001.png?type=thumb&opt=C72x72.fwebp"/>
+                                            <CateBox>보도자료</CateBox>
+                                            <DateBox>
+                                                2025.07.31
+                                            </DateBox>
+                                        </BadgeBox>
+                                    </BoxTop>
+                                    <TitleBox>
+                                        <TitleBoxLink>카카오프렌즈, 여름 한정 테마 '냥냥특집' 테마 굿즈 출시</TitleBoxLink>
+                                    </TitleBox>
+                                    <TodayNewsBtnShareIcoComm>
+                                    <svg data-v-19bdf39b="" data-v-76d2af19="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico_share"><g data-v-19bdf39b="" fill="none" fill-rule="evenodd"><path data-v-19bdf39b="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-19bdf39b="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill" fill="black"><circle data-v-19bdf39b="" cx="2" cy="2" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="10" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                    </TodayNewsBtnShareIcoComm>
+                                </ItemCardNewBoxNews>
+                            </TodayNewsSlickSlide>
+                            <TodayNewsSlickSlide>
+                                <ItemCardNewBoxNews>
+                                    <BoxTop>
+                                        <BadgeBox>
+                                            <IconBox src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/category/1ee6415a019300001.png?type=thumb&opt=C72x72.fwebp"/>
+                                            <CateBox>보도자료</CateBox>
+                                            <DateBox>
+                                                2025.07.31
+                                            </DateBox>
+                                        </BadgeBox>
+                                    </BoxTop>
+                                    <TitleBox>
+                                        <TitleBoxLink>카카오임팩트, ‘돕는 AI 컨퍼런스 2025’ 참가자 모집</TitleBoxLink>
+                                    </TitleBox>
+                                    <TodayNewsBtnShareIcoComm>
+                                    <svg data-v-19bdf39b="" data-v-76d2af19="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="ico_share"><g data-v-19bdf39b="" fill="none" fill-rule="evenodd"><path data-v-19bdf39b="" d="M0 0H24V24H0z" transform="translate(-350 -222) translate(24 192) translate(326 30)"></path> <g data-v-19bdf39b="" transform="translate(-350 -222) translate(24 192) translate(326 30) translate(10 2)" class="fill" fill="black"><circle data-v-19bdf39b="" cx="2" cy="2" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="10" r="2"></circle> <circle data-v-19bdf39b="" cx="2" cy="18" r="2"></circle></g></g></svg>
+                                    </TodayNewsBtnShareIcoComm>
+                                </ItemCardNewBoxNews>
+                            </TodayNewsSlickSlide>
+                        </TodayNewsSlickTrack>
+                    </WrapSlides>
+                    <WrapLinks>
+                        <LinkNewsDiv>
+                            <LinkNews>
+                                <TitleLink>보도자료</TitleLink>
+                                <svg data-v-be739f74="" data-v-85d56d7e="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="btn_arr"><circle data-v-be739f74="" cx="12" cy="12" r="12" fill="black"></circle> <path data-v-be739f74="" d="M13.9704 8.57847L13.2078 9.34107L15.7207 11.8539L7.53764 11.8543L7.53765 12.9296L15.7207 12.9292L13.2079 15.442L13.9705 16.2046L17.7836 12.3916L13.9704 8.57847Z" fill="white"></path></svg>
+                            </LinkNews>
+                        </LinkNewsDiv>
+                        <LinkNewsDiv>
+                            <LinkNews>
+                                <TitleLink>미디어자료</TitleLink>
+                                <svg data-v-be739f74="" data-v-85d56d7e="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="btn_arr"><circle data-v-be739f74="" cx="12" cy="12" r="12" fill="black"></circle> <path data-v-be739f74="" d="M13.9704 8.57847L13.2078 9.34107L15.7207 11.8539L7.53764 11.8543L7.53765 12.9296L15.7207 12.9292L13.2079 15.442L13.9705 16.2046L17.7836 12.3916L13.9704 8.57847Z" fill="white"></path></svg>
+                            </LinkNews>
+                        </LinkNewsDiv>
+                    </WrapLinks>
+                </WrapBottom>
+            </InnerMainInnerNews>
+        </ContMainAreaNewsContainer>
     )
 }
 
--- src/styles/main/ContMainAreaNews.styles.js
+++ src/styles/main/ContMainAreaNews.styles.js
@@ -4,6 +4,474 @@ export const ContMainAreaNewsContainer = styled.div`
     max-width: 1316px;
     width: 100%;
     height: 1100px;
-    background-color: green;
     margin: 120px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const TitleContTypeNewsTitNews3 = styled.div`
+    max-width: 1316px;
+    width: 100%;
+    max-height: 72px;
+    height: 100%;
+    font-size: 48px;
+    line-height: 72px;
+    letter-spacing: -1px;
+    color: #000;
+    font-weight: 400;
+
+    &::before {
+        content: "";
+        max-width: 72px;
+        width: 100%;
+        max-height: 72px;
+        height: 100%;
+        margin-right: 16px;
+        display: inline-block;
+        vertical-align: top;
+        background-size: 100%;
+        background-image: url(//t1.kakaocdn.net/kakaocorp/kakaocorp/service/calendar/light/ico_date3.gif);
+    
+    }
+`
+
+export const InnerMainInnerNews = styled.div`
+    max-width: 1315px;
+    width: 100%;
+    max-height: 985px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const WrapTop = styled.div`
+    max-width: 1332px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+`
+
+export const SlickSlider = styled.div`
+    position: relative;
+    max-width: 1332px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+    box-sizing: border-box;
+    user-select: none;
+    line-height: 1.5;
+    font-weight: 400;
+`
+
+export const SlickList = styled.div`
+    max-width: 1332px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+`
+
+export const SlickTrack = styled.div`
+    max-width: 1332px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+    opacity: 1;
+    transform: translate3d(0px, 0px, 0px);
+    position: relative;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+
+
+    &::before {
+        display: table;
+        content: "";
+    }
+
+    &::after {
+        clear: both;
+        display: table;
+        content: "";
+    }
+`
+
+export const SlickSlide = styled.div`
+    max-width: 444px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+`
+
+export const SlickSlideDiv1 = styled.div`
+    max-width: 444px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+`
+
+export const SlickSlideDiv2 = styled.div`
+    max-width: 444px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+`
+
+export const WrapCardInnerSlide = styled.div`
+    max-width: 428px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+`
+
+export const ItemCardNewItemNormalCardTypeMain = styled.div`
+    max-width: 428px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+    background-color: #f3f3f3;
+    position: relative;
+    border-radius: 36px;
+    box-sizing: border-box;
+    overflow: hidden;
+`
+
+export const InnerCard = styled.div`
+    max-width: 428px;
+    width: 100%;
+    max-height: 621px;
+    height: 100%;
+    position: relative;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 36px;
+
+`
+
+export const InfoCate = styled.div`
+    margin: 25px auto;
+    max-width: 368px;
+    width: 100%;
+    max-height: 46px;
+    height: 100%;
+    font-size: 0;
+    display: flex;
+    align-items: center;
+`
+
+export const TxtCate = styled.div`
+    max-width: 83px;
+    width: 100%;
+    max-height: 41px;
+    width: 100%;
+    background-color: #fff;
+    line-height: 40px;
+    border-radius: 8px;
+    font-size: 14px;
+    letter-spacing: -.2px;
+    color: #000;
+    display: flex;
+    justify-content: center;
+`
+
+export const TxtDate = styled.div`
+    max-width: 76px;
+    width: 100%;
+    max-height: 46px;
+    height: 100%;
+    margin-left: 12px;
+    font-size: 14px;
+    line-height: 46px;
+    letter-spacing: -.2px;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+`
+
+export const LinkItemDiv = styled.div`
+    max-width: 428px;
+    width: 100%;
+    max-height: 525px;
+    height: 100%;
+    
+`
+
+export const LinkItem = styled.a`
+    max-width: 428px;
+    width: 100%;
+    max-height: 525px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const InfoCard = styled.div`
+    max-width: 368px;
+    width: 100%;
+    max-height: 26px;
+    height: 100%;
+    position: absolute;
+    top: 326px;
+    left: 30px;
+    right: 30px;
+    overflow: hidden;
+    font-size: 16px;
+    line-height: 26px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    pointer-events: none;
+`
+
+export const TxtKeyword = styled.span`
+    max-width: 94px;
+    width: 100%;
+    max-height: 23px;
+    height: 100%;
+    margin-right: 8px;
+    letter-spacing: -.5px;
+    font-size: 16px;
+    white-space: nowrap;
+    color: #666;
+`
+
+
+export const TitCard = styled.div`
+    max-width: 428px;
+    width: 100%;
+    max-height: 210px;
+    height: 100%;
+    padding: 0 30px;
+    font-size: 30px;
+    font-weight: bold;
+    line-height: 1.4;
+    user-select: none;
+    letter-spacing: -.6px;
+    color: #000;
+`
+
+export const WrapThumb = styled.div`
+    max-width: 428px;
+    width: 100%;
+    max-height: 249px;
+    height: 100%;
+    margin-top: 66px;
+    position: relative;
+`
+
+export const ThumbImg = styled.img`
+    max-width: 443px;
+    width: 100%;
+    max-height: 249px;
+    height: 100%;
+    position: absolute;
+    overflow-clip-margin: content-box;
+    overflow: clip;
+
+`
+
+
+export const BtnShareIcoComm = styled.div`
+    max-width: 24px;
+    width: 100%;
+    max-height: 24px;
+    height: 100%;
+    position: absolute;
+    right: 30px;
+    top: 41px;
+    z-index: 20;
+    overflow: hidden;
+
+`
+
+
+export const IcoCate = styled.img`
+    max-width: 46px;
+    width: 100%;
+    max-height: 46px;
+    height: 100%;
+    margin-right: 12px;
+    vertical-align: top;
+`
+
+
+
+export const WrapBottom = styled.div`
+    max-width: 1316px;
+    width: 100%;
+    max-height: 324px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const WrapSlides = styled.div`
+    max-width: 1332px;
+    width: 100%;
+    max-height: 198px;
+    height: 100%;
+`
+
+export const TodayNewsSlickTrack = styled.div`
+    max-width: 1332px;
+    width: 100%;
+    max-height: 198px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    top: 0;
+    left: 0;
+`
+
+export const TodayNewsSlickSlide = styled.div`
+    max-width: 666px;
+    width: 100%;
+    max-height: 198px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    
+`
+
+export const ItemCardNewBoxNews = styled.div`
+    position: relative;
+    max-width: 650px;
+    width: 100%;
+    max-height: 198px;
+    height: 100%;
+    background-color: #f3f3f3;
+    border-radius: 20px;
+`
+
+export const BoxTop = styled.div`
+    margin: 30px auto;
+    max-width: 578px;
+    width: 100%;
+    max-height: 38px;
+    height: 100%;
+`
+export const TitleBox = styled.div`
+    margin: 0px auto;
+    font-size: 27px;
+    font-weight: 400;
+    line-height: 38px;
+    color: #000;
+    max-width: 578px;
+    width: 100%;
+    max-height: 76px;
+    height: 100%;
+`
+
+export const TitleBoxLink = styled.a`
+    max-width: 578px;
+    width: 100%;
+    max-height: 76px;
+    height: 100%;
+`
+
+export const BadgeBox = styled.div`
+    max-width: 217px;
+    width: 100%;
+    max-height: 38px;
+    height: 100%;
+    font-size: 14px;
+    letter-spacing: -.2px;
+    user-select: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const IconBox = styled.img`
+    max-width: 38px;
+    width: 100%;
+    max-height: 38px;
+    height: 100%;
+`
+
+export const CateBox = styled.div`
+    max-width: 73px;
+    width: 100%;
+    max-height: 33px;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 8px;
+    color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const DateBox = styled.div`
+    max-width: 76px;
+    width: 100%;
+    max-height: 38px;
+    height: 100%;
+    font-size: 14px;
+    line-height: 38px;
+    color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const TodayNewsBtnShareIcoComm = styled.div`
+    position: absolute;
+    top: 38px;
+    right: 34px;
+    max-width: 24px;
+    width: 100%;
+    max-height: 24px;
+    height: 100%;
+`
+
+
+export const WrapLinks = styled.div`
+    max-width: 1332px;
+    width: 100%;
+    max-height: 110px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const LinkNewsDiv = styled.div`
+    max-width: 650px;
+    width: 100%;
+    max-height: 110px;
+    height: 100%;
+    background-color: #f3f3f3;
+    box-sizing: border-box;
+    border-radius: 20px;
+`
+
+export const LinkNews = styled.a`
+    max-width: 650px;
+    width: 100%;
+    max-height: 110px;
+    height: 100%;
+    display: flex;
+    gap: 450px;
+    justify-content: center;
+    align-items: center;
+
+`
+
+export const TitleLink = styled.div`
+    font-size: 26px;
+    font-weight: 400;
+    line-height: 38px;
+    letter-spacing: -.6px;
 `
