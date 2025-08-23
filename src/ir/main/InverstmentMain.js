0316b29bd06a021c0ed14ad355ab49a3119a1ad4
pan6603 <pan5158a@gmail.com>
2025-08-24 01:48:34 +0900

    카카오 주요현황 레이아웃 구현

--- src/ir/main/InverstmentMain.js
+++ src/ir/main/InverstmentMain.js
@@ -12,14 +12,150 @@ import {
     WrapRelationThird,
     TitRelation,
     TxtSub,
-    TxtGoto
+    TxtGoto,
+    SectionNewsTitle,
+    WrapBoard,
+    WrapNews,
+    LinkAllDiv,
+    LinkAll,
+    ListNews,
+    LinkNewsDiv,
+    InnerNews,
+    BoxInfo,
+    BadgeNews,
+    TxtDate,
+    TitNews,
+    ListTag,
+    ListTagItem,
+    BoxBoard,
+    TitBoard,
+    ListBoard,
+    LinkBoard,
+    InnerBoard,
+    TxtSubDiv,
+    TxtEm,
+    BorderLine,
+    TxtSubDate
 } from "../../styles/ir/main/InverstmentMain.styles"
 
 function InverstmentMain() {
     return (
         <DocMain>
             <SectionVisual>카카오 비전</SectionVisual>
-            <SectionNews>카카오 주요현황</SectionNews>
+            <SectionNews>
+                <SectionNewsTitle>카카오 주요현황</SectionNewsTitle>
+                <WrapBoard>
+                    <BoxBoard>
+                        <TitBoard>주주총회</TitBoard>
+                        <ListBoard>
+                            <LinkBoard>
+                                <InnerBoard>
+                                    <TxtEm>주주총회</TxtEm>
+                                    <TxtSubDiv>바로가기</TxtSubDiv>
+                                </InnerBoard>
+                            </LinkBoard>
+                            <BorderLine />
+                            <LinkBoard>
+                                <InnerBoard>
+                                    <TxtEm>주주환원</TxtEm>
+                                    <TxtSubDiv>바로가기</TxtSubDiv>
+                                </InnerBoard>
+                            </LinkBoard>
+                        </ListBoard>
+                    </BoxBoard>
+                    <BoxBoard>
+                        <TitBoard>IR 행사</TitBoard>
+                        <ListBoard>
+                            <LinkBoard>
+                                <InnerBoard>
+                                    <TxtEm>다올투자증권 국내 NDR</TxtEm>
+                                    <TxtSubDate>2025-08-18</TxtSubDate>
+                                </InnerBoard>
+                            </LinkBoard>
+                            <BorderLine />
+                            <LinkBoard>
+                                <InnerBoard>
+                                    <TxtEm>메리츠증권 국내 NDR</TxtEm>
+                                    <TxtSubDate>2025-08-13</TxtSubDate>
+                                </InnerBoard>
+                            </LinkBoard>
+                        </ListBoard>
+                    </BoxBoard>
+                    <BoxBoard>
+                        <TitBoard>최근 제출 보고서</TitBoard>
+                        <ListBoard>
+                            <LinkBoard>
+                                <InnerBoard>
+                                    <TxtEm>감사(검토)보고서</TxtEm>
+                                    <TxtSubDiv>바로가기</TxtSubDiv>
+                                </InnerBoard>
+                            </LinkBoard>
+                            <BorderLine />
+                            <LinkBoard>
+                                <InnerBoard>
+                                    <TxtEm>사업(분·반기)보고서</TxtEm>
+                                    <TxtSubDiv>바로가기</TxtSubDiv>
+                                </InnerBoard>
+                            </LinkBoard>
+                        </ListBoard>
+                    </BoxBoard>
+                </WrapBoard>
+                <WrapNews>
+                    <ListNews>
+                        <LinkNewsDiv>
+                            <InnerNews>
+                                <BoxInfo>
+                                    <BadgeNews>보도자료</BadgeNews>
+                                    <TxtDate>2025.08.21</TxtDate>
+                                </BoxInfo>
+                                <TitNews>카카오, ‘2025 찾아가는 카카오 클래스’ 오프라인 과정 참가자 모집</TitNews>
+                                <ListTag>
+                                    <ListTagItem>#카카오클래스</ListTagItem>
+                                    <ListTagItem>#카카오</ListTagItem>
+                                    <ListTagItem>#카카오채널</ListTagItem>
+                                </ListTag>
+                            </InnerNews>
+                        </LinkNewsDiv>
+                        <LinkNewsDiv>
+                            <InnerNews>
+                                <BoxInfo>
+                                    <BadgeNews>보도자료</BadgeNews>
+                                    <TxtDate>2025.08.20</TxtDate>
+                                </BoxInfo>
+                                <TitNews>
+                                    카카오, ‘사이좋은 디지털 세상’ 10주년 맞아 AI 맞춤형 교육으로 개편···
+                                    <br />
+                                    2025년 2학기 참여 학교 ...
+                                </TitNews>
+                                <ListTag>
+                                    <ListTagItem>#더가깝게카카오</ListTagItem>
+                                    <ListTagItem>#사이좋은디지털세상</ListTagItem>
+                                    <ListTagItem>#카카오 ...</ListTagItem>                
+                                </ListTag>
+                            </InnerNews>
+                        </LinkNewsDiv>
+                        <LinkNewsDiv>
+                            <InnerNews>
+                                <BoxInfo>
+                                    <BadgeNews>보도자료</BadgeNews>
+                                    <TxtDate>2025.08.14</TxtDate>
+                                </BoxInfo>
+                                <TitNews>
+                                    카카오, ‘카카오테크 캠퍼스 아이디어톤’으로 전국 5개 국립대 손잡고 지역 기술 인재 양성 박차
+                                </TitNews>
+                                <ListTag>
+                                    <ListTagItem>#ESG</ListTagItem>
+                                    <ListTagItem>#기술인재양성</ListTagItem>
+                                    <ListTagItem>#카카오테크 캠퍼스</ListTagItem>                
+                                </ListTag>
+                            </InnerNews>
+                        </LinkNewsDiv>
+                    </ListNews>
+                </WrapNews>
+                <LinkAllDiv>
+                    <LinkAll>보도자료 전체보기</LinkAll>
+                </LinkAllDiv>
+            </SectionNews>
             <SectionRelation>
                 <SectionRelationTitle>필요한 미래를 만드는 카카오</SectionRelationTitle>
                 <ListRelation>
--- src/styles/ir/main/InverstmentMain.styles.js
+++ src/styles/ir/main/InverstmentMain.styles.js
@@ -174,9 +174,268 @@ export const SectionNews = styled.section`
     max-width: 1316px;
     width: 100%;
     height: 1014px;
-    background-color: red;
     margin: 120px auto;
 `
+
+export const SectionNewsTitle = styled.h4`
+    max-width: 1316px;
+    width: 100%;
+    height: 72px;
+    margin-bottom: 46px;
+    font-size: 48px;
+    color: #000;
+    font-weight: 400;
+    letter-spacing: -1px;
+    line-height: 1.5;
+
+    &::before {
+        content: '';
+        width: 72px;
+        height: 72px;
+        margin-right: 16px;
+        display: inline-block;
+        vertical-align: top;
+        background: url(https://t1.kakaocdn.net/kakaocorp/Ir/images/img_balloon.png) center/contain no-repeat;
+    }
+`
+
+export const WrapBoard = styled.div`
+    max-width: 1316px;
+    width: 100%;
+    height: 241px;
+    margin-bottom: 46px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const BoxBoard = styled.div`
+    max-width: 428px;
+    width: 100%;
+    height: 241px;
+    background-color: red;
+    padding: 40px 30px 24px;
+    box-sizing: border-box;
+    overflow: hidden;
+    border-radius: 24px;
+    background: #f3f3f3;
+`
+
+export const TitBoard = styled.div`
+    margin-bottom: 16px;
+    font-size: 30px;
+    letter-spacing: -0.8px;
+    color: #000;
+    font-weight: 700;
+`
+
+
+export const ListBoard = styled.div`
+    max-width: 368px;
+    width: 100%;
+    height: 116px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const LinkBoard = styled.a`
+    max-width: 368px;
+    width: 100%;
+    height: 57px;
+`
+
+export const InnerBoard = styled.div`
+    max-width: 368px;
+    width: 100%;
+    height: 57px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const TxtSubDiv = styled.div`
+    text-decoration: underline;
+    text-underline-offset: 4px;
+    letter-spacing: -0.5px;
+    font-size: 17px;
+    font-weight: 700;
+`
+
+export const TxtSubDate = styled.div`
+    text-decoration: none;
+    color: #666;
+    text-underline-offset: 4px;
+    font-size: 17px;
+    letter-spacing: -0.5px;
+`
+
+
+export const BorderLine = styled.div`
+    border: 1px solid #D0D0D0;
+    max-width: 368px;
+    width: 100%;
+    height: 1px;
+`
+
+
+export const TxtEm = styled.em`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    letter-spacing: -0.5px;
+    color: #000;
+`
+
+
+
+export const WrapNews = styled.div`
+    max-width: 1316px;
+    width: 100%;
+    height: 360px;
+    margin-bottom: 46px;
+`
+
+export const ListNews = styled.div`
+    max-width: 1316px;
+    width: 100%;
+    height: 360px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const LinkNewsDiv = styled.div`
+    max-width: 428px;
+    width: 100%;
+    height: 360px;
+    border-radius: 24px;
+`
+
+export const LinkNewsDivItem = styled.a`
+    max-width: 428px;
+    width: 100%;
+    height: 360px;
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+    font-size: 14px;
+    overflow: hidden;
+    border-radius: 24px;
+`
+
+export const InnerNews = styled.div`
+    max-width: 428px;
+    width: 100%;
+    height: 360px;
+    padding: 36px 36px 32px;
+    display: flex;
+    flex-direction: column;
+    padding: 40px 40px 32px;
+    background: #f3f3f3;
+    box-sizing: border-box;
+    border-radius: 24px;
+`
+
+export const BoxInfo = styled.div`
+    max-width: 356px;
+    width: 100%;
+    height: 46px;
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    &::before {
+        width: 46px;
+        height: 46px;
+        background: url(https://t1.kakaocdn.net/kakaocorp/Ir/images/img_notice.png) center / contain no-repeat;
+        content: '';
+    }
+`
+
+export const TitNews = styled.div`
+    max-width: 356px;
+    width: 100%;
+    height: max-content;;
+    margin-top: 20px;
+    font-size: 30px;
+    font-weight: 700;
+    overflow: hidden;
+`
+
+export const ListTag = styled.div`
+    max-width: 356px;
+    width: 100%;
+    height: 17px;
+    overflow: hidden;
+    margin-top: auto;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    line-height: 1.1;
+    display: flex;
+    gap: 8px;
+`
+
+export const ListTagItem = styled.div`
+    font-size: 16px;
+    color: #666;
+    white-space: nowrap;
+    width: max-content;
+    height: 23px;
+`
+
+
+export const BadgeNews = styled.div`
+    max-width: 81px;
+    width: 100%;
+    height: 37px;
+    background-color: #fff;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const TxtDate = styled.div`
+    font-size: 14px;
+    letter-spacing: -0.2px;
+    color: #000;
+
+`
+
+
+
+export const LinkAllDiv = styled.div`
+    max-width: 315px;
+    width: 100%;
+    height: 79px;
+    margin: 0 auto;
+    background-color: blue;
+    border-radius: 36px;
+    background: #f3f3f3;
+    font-size: 22px;
+    letter-spacing: -0.6px;
+`
+
+export const LinkAll = styled.a`
+    width: fit-content;
+    height: 79px;
+    margin: 0 auto;
+    padding: 23px 61px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &::after {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        margin: 0px 0 0 12px;
+        background: url(https://t1.kakaocdn.net/kakaocorp/Ir/images/ico_arrow.svg) no-repeat;
+        content: '';
+    }
+`
+
+
 
 export const SectionVisual = styled.section`
     max-width: 1316px;
