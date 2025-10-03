e84946b51015f8fdb2f6a2dc766a065ba04fa66a
pan6603 <pan5158a@gmail.com>
2025-10-03 18:05:40 +0900

    Footer 컴포넌트 추가

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -1,23 +1,101 @@
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import React, { useState } from 'react'; 
 import { 
     MainContent, 
     ContentArticle,
     TitMain,
-
+    EmphTit,
+    TxtTit,
+    SectionService,
+    WrapTabs,
+    ListTab,
+    LinkTabDiv,
+    LinkTab
 } from "../../styles/pages/service/service.styles";
 
 
 function Service() {
+    const [TabMenu, setTabMenu] = useState("entire");
+
+    const CategoryTabs = (menu) => {
+        setTabMenu(menu);     
+    }
+
     return (
         <>
             <Header />
             <MainContent>
                 <ContentArticle>
                     <TitMain>
-
+                        <EmphTit>서비스</EmphTit>
+                        <TxtTit>더 나은 세상을 만드는 카카오</TxtTit>
                     </TitMain>
+                    <SectionService>
+                        <WrapTabs>
+                            <ListTab>
+                                <LinkTabDiv>
+                                    <LinkTab 
+                                        onClick={() => CategoryTabs("entire")}
+                                        isActive={TabMenu === "entire"} 
+                                        >
+                                        전체
+                                        </LinkTab>
+                                </LinkTabDiv>
+                                <LinkTabDiv>
+                                    <LinkTab
+                                        onClick={() => CategoryTabs("communication")}
+                                        isActive={TabMenu === "communication"} 
+                                    >
+                                    커뮤니케이션
+                                    </LinkTab>
+                                </LinkTabDiv>
+                                <LinkTabDiv>
+                                    <LinkTab
+                                        onClick={() => CategoryTabs("life")}
+                                        isActive={TabMenu === "life"}
+                                    >
+                                    일상 편의 
+                                    </LinkTab>
+                                </LinkTabDiv>
+                                <LinkTabDiv>
+                                    <LinkTab 
+                                         onClick={() => CategoryTabs("business")}
+                                         isActive={TabMenu === "business"}
+                                    >
+                                    비즈니스
+                                    </LinkTab>
+                                </LinkTabDiv>
+                                <LinkTabDiv> 
+                                    <LinkTab 
+                                        onClick={() => CategoryTabs("shopping")}
+                                        isActive={TabMenu === "shopping"}
+                                    >
+                                    쇼핑
+                                    </LinkTab>
+                                </LinkTabDiv>
+                                <LinkTabDiv>
+                                    <LinkTab 
+                                        onClick={() => CategoryTabs("entertainment")}
+                                        isActive={TabMenu === "entertainment"}
+                                    >
+                                    엔터테인먼트
+                                    </LinkTab>
+                                </LinkTabDiv>
+                                <LinkTabDiv>
+                                    <LinkTab
+                                        onClick={() => CategoryTabs("social_impact")}
+                                        isActive={TabMenu === "social_impact"}
+                                    >
+                                    소셜임팩트
+                                    </LinkTab>
+                                </LinkTabDiv>
+                            </ListTab>
+                        </WrapTabs>
+                    </SectionService>
                 </ContentArticle>
             </MainContent>
+            <Footer />
         </>
     )
 }
--- src/styles/pages/service/service.styles.js
+++ src/styles/pages/service/service.styles.js
@@ -1,19 +1,19 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 
 export const MainContent = styled.div`
     max-width: 1440px;
     width: 100%;
-    height: 8089px;
+    height: 3247px;
     margin: 91px auto;
 `
 
 export const ContentArticle = styled.article`
     max-width: 1296px;
     width: 100%;
-    height: 8089px;
+    height: 3247px;
     margin: 0 auto;
-    position: relative;
     padding-bottom: 182px;
     background-color: red;
 `
@@ -22,17 +22,78 @@ export const TitMain = styled.div`
     max-width: 1296px;
     width: 100%;
     height: 220px;
-    background-color: green;
     margin: 0 auto;
-    // background-image: url(//t1.kakaocdn.net/kakaocorp/kakaocorp/service/ico_tit_service_dark.gif);
-    // background-repeat: no-repeat;
-    // background-position: 0 94px;
-    // background-size: 72px 72px;
+    background-image: url(//t1.kakaocdn.net/kakaocorp/kakaocorp/service/ico_tit_service.gif);
+    background-repeat: no-repeat;
+    background-position: 0 94px;
+    background-size: 72px 72px;
     letter-spacing: -1px;
-    color: #fff;
+    color: #000;
     font-size: 46px;
     line-height: 66px;
+    padding-top: 96px;
+    box-sizing: border-box;
+    background-color: green;
+`
+
+export const EmphTit = styled.div`
+    display: block;
+    padding-left: 84px;
+    font-weight: 700;
+`
+
+export const TxtTit = styled.div`
+    display: block;
+    margin-top: 22px;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 36px;
 `
 
 
+export const SectionService = styled.section`
+    position: relative;
+    margin-top: 92px;
+    max-width: 1296px;
+    width: 100%;
+    height: 2763px;
+    background-color: blue;
+    color: #333;
+`
 
+export const WrapTabs = styled.div`
+    max-width: 1296px;
+    width: 100%;
+    height: 61px;
+`
+
+export const ListTab = styled.div`
+    max-width: 1296px;
+    width: 100%;
+    height: 61px;
+    display: flex;
+    align-items: center;
+`
+
+export const LinkTabDiv = styled.div`
+    width: max-content;
+    height: 51px;
+    margin: 10px 8px 0 0;
+    display: flex;
+`
+
+export const LinkTab = styled(Link)`
+    width: max-content;
+    height: 51px;
+    padding: 12px 24px;
+    box-sizing: border-box;
+    border-radius: 999px;
+    background: #f3f3f3;
+    font-size: 17px;
+    line-height: 27px;
+    letter-spacing: -.5px;
+    color: #666;
+    text-decoration: none;
+    background-color: ${({ isActive }) => isActive ? "#000" : "#f3f3f3"};
+    color: ${({ isActive }) => isActive ? "#fff" : "#666"};
+`
