b1a230cd26a112de6b3603c8f208508b401612c5
pan6603 <pan5158a@gmail.com>
2025-11-18 15:20:31 +0900

    Overlay 추가

--- src/styles/ir/main/InverstmentHeader.styles.js
+++ src/styles/ir/main/InverstmentHeader.styles.js
@@ -1,6 +1,16 @@
 import styled from "styled-components";
 
 
+export const Overlay = styled.div`
+    position: fixed;       
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5); 
+    z-index: 998;         
+`;
+
 export const DocHeader = styled.header`
     position: sticky;
     top: 0;
